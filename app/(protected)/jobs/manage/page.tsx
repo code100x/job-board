6e9387c1a526274a7f526df71dfccab72e3b9300
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-08-07 01:32:23 +0530

    jobs/manage/page updated and ManageJobsCard created

--- app/(protected)/jobs/manage/page.tsx
+++ app/(protected)/jobs/manage/page.tsx
@@ -1,21 +1,32 @@
 import { auth } from "@/auth";
 import NewJobModal from "@/components/NewJobModal";
 import { redirect } from "next/navigation";
+import ManageJobsCard from "@/components/ManageJobsCard";
+import { prisma } from "@/lib/db";
+import { cn } from "@/lib/utils";
 
 const ManageJobsPage = async () => {
-
     const session = await auth();
     const role = session?.user.role;
 
-    if(role !== "ADMIN") {
-        redirect("/")
+    if (role !== "ADMIN") {
+        redirect("/");
     }
-
+    const jobs = await prisma.job.findMany({});
+    console.log("jobs", jobs);
     return (
-        <div className="w-full flex-grow flex justify-center items-center p-2">
-            <NewJobModal />
+        <div className="w-1/2 flex-grow justify-center items-center p-2 flex flex-col">
+            <div className="flex flex-row gap-8 items-center w-full mb-4">
+                <p className="text-3xl font-semibold">Manage your Jobs</p>
+                <NewJobModal />
+            </div>
+            <div className={cn("jobs flex flex-col max-h-[420px] gap-3 overflow-y-scroll w-full")}>
+                {jobs.map((job) => {
+                    return <ManageJobsCard job={job} key={job.id} />;
+                })}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default ManageJobsPage;
