5d722cbf2910308a16f1d3c175041ae729019d52
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-08-06 23:59:52 +0530

    all merge conflicts resolved

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -6,6 +6,16 @@ import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import {Button} from "@/components/ui/button";
 import {toast} from "@/components/ui/use-toast";
+import {
+  DropdownMenu,
+  DropdownMenuContent, DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import UserImage from "@/components/UserImage";
+import {LogOut, UserRound} from "lucide-react";
+import {signOut} from "next-auth/react";
 
 type NavbarProps = {
   session: Session | null;
@@ -23,6 +33,14 @@ const Navbar = ({ session }: NavbarProps) => {
     {
       name: "Explore",
       route: "/jobs",
+    },
+  ];
+
+  const dropDownData = [
+    {
+      name: "Profile",
+      icon: <UserRound size={15} />,
+      href: "/profile",
     },
   ];
 
@@ -78,18 +96,71 @@ const Navbar = ({ session }: NavbarProps) => {
         </div>
       </div>
 
-      {!session ? (
-        <Link href="/login">
-          <Button className="font-medium">Join Now</Button>
-        </Link>
-      ) : (
-        <Button
-          onClick={handleSignOut}
-          variant="outline"
-          className="font-medium rounded-md"
-        >
-          Sign Out
-        </Button>
+      {session && session?.user && (
+          <DropdownMenu>
+            <DropdownMenuTrigger className="w-[2rem] flex items-center p-[0.2rem]  justify-center h-[2rem]">
+              {!session?.user.image ? (
+                  <div className="p-1 border-2 rounded-md">
+                    <UserRound />
+                  </div>
+              ) : (
+                  <UserImage image={session?.user.image} />
+              )}
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent className="!w-[15rem] dark:shadow-[#030712] translate-y-8 scale-110 -translate-x-10 shadow-lg bg-white">
+              <DropdownMenuLabel className="flex gap-4 items-center">
+                <div className="!w-[2rem] flex items-center p-[0.2rem]  justify-center !h-[2rem]">
+                  {!session?.user.image ? (
+                      <div className="p-1 border-2 rounded-full border-[#1a1a1a]">
+                        <UserRound />
+                      </div>
+                  ) : (
+                      <UserImage image={session?.user.image} />
+                  )}
+                </div>
+
+                <div className="flex flex-col">
+                  <span className="max-w-[200px]">{session?.user?.name}</span>
+                  <span className="text-[0.8rem] max-w-[200px] text-gray-400">
+                    {session?.user?.email}
+                  </span>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+
+              {dropDownData.map((item, index) => {
+                return (
+                    <DropdownMenuItem
+                        className="flex gap-2 focus:border-gray-500 cursor-pointer"
+                        onClick={() => router.push("/profile")}
+                        key={index}
+                    >
+                      <span>{item.icon}</span>
+                      <span>{item.name}</span>
+                    </DropdownMenuItem>
+                );
+              })}
+              <DropdownMenuSeparator />
+              {session?.user && (
+                  <DropdownMenuItem
+                      onClick={async () => {
+                        await signOut();
+                        router.push("/");
+                      }}
+                      className="flex gap-2 cursor-pointer"
+                  >
+                    <LogOut size={15} />
+                    Logout
+                  </DropdownMenuItem>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
+      )}
+      {!session && (
+          <Link href="/login">
+            <Button className="font-medium">Join Now</Button>
+          </Link>
       )}
     </nav>
   );
