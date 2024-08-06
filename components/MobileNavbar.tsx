5a20bca97671a948186ced5928ceb10be5092f12
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-08-06 15:59:32 +0530

    Updated the Menu Design

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -7,12 +7,19 @@ import { toast } from "./ui/use-toast";
 import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import {
-  DropdownMenu,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { AlignJustify } from "lucide-react";
 import { DropdownMenuContent } from "@radix-ui/react-dropdown-menu";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 type NavbarProps = {
   session: Session | null;
 };
@@ -52,15 +59,15 @@ export const MobileNavbar = ({ session }: NavbarProps) => {
       <h3 className="text-xl bg-gradient-to-r from-indigo-600 via-violet-500 to-blue-700 bg-clip-text text-transparent font-black">
         100xJobs
       </h3>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
+      <Sheet>
+        <SheetTrigger>
           <AlignJustify />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="mr-4 mt-2 text-gray-500 z-1000 bg-white rounded-xl  border-black shadow-md bo flex flex-col py-4 px-2  w-fit  justify-evenly gap-4 text-base font-semibold">
-          {navItems.map((item) => {
-            return (
-              <DropdownMenuItem key={item.name} className="cursor-pointer font-medium rounded-md flex justify-center items-center">
-                <Link href={item.route}>
+        </SheetTrigger>
+        <SheetContent side="right" className="w-64 bg-white p-6">
+          <nav className="grid gap-4 text-gray-500 font-semibold tracking-tighter">
+            {navItems.map((item) => {
+              return (
+                <Link key={item.name} href={item.route}>
                   <p
                     className={cn("cursor-pointer hover:text-gray-900", {
                       "text-gray-900": pathName === item.route,
@@ -69,11 +76,9 @@ export const MobileNavbar = ({ session }: NavbarProps) => {
                     {item.name}
                   </p>
                 </Link>
-              </DropdownMenuItem>
-            );
-          })}
-          {userRole === "ADMIN" ? (
-            <DropdownMenuItem className="cursor-pointer font-medium rounded-md  flex justify-center items-center ">
+              );
+            })}
+            {userRole === "ADMIN" ? (
               <Link href="/jobs/manage">
                 <p
                   className={cn("cursor-pointer hover:text-gray-900", {
@@ -83,30 +88,23 @@ export const MobileNavbar = ({ session }: NavbarProps) => {
                   Manage
                 </p>
               </Link>
-            </DropdownMenuItem>
-          ) : null}
-          {!session ? (
-            <DropdownMenuItem className="cursor-pointer font-medium rounded-md flex justify-center items-center">
+            ) : null}
+            {!session ? (
               <Link href="/login">
-                <Button className="font-medium">Join Now</Button>
+                <Button className="font-medium text-lg">Join Now</Button>
               </Link>
-            </DropdownMenuItem>
-          ) : (
-            <DropdownMenuItem
-              onClick={handleSignOut}
-              className="cursor-pointer flex justify-center font-medium rounded-md items-center"
-            >
+            ) : (
               <Button
                 onClick={handleSignOut}
                 variant="outline"
-                className="font-medium rounded-md"
+                className="font-medium rounded-md text-lg"
               >
                 Sign Out
               </Button>
-            </DropdownMenuItem>
-          )}
-        </DropdownMenuContent>
-      </DropdownMenu>
+            )}
+          </nav>
+        </SheetContent>
+      </Sheet>
     </nav>
   );
 };
