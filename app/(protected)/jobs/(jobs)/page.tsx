74f841dae8dc4aa9c76cd0514cf6b2ee6eeb3bd7
devansh <devanshverma193@gmail.com>
2024-08-07 12:47:18 +0530

    features-added

--- app/(protected)/jobs/(jobs)/page.tsx
+++ app/(protected)/jobs/(jobs)/page.tsx
@@ -1,23 +1,36 @@
+"use client";
+import { getJobs } from "@/actions/job";
 import JobCard from "@/components/JobCard";
 import Sidebar from "@/components/Sidebar";
 import { JobDisplay } from "@/components/job-display";
 import { prisma } from "@/lib/db";
 import { cn } from "@/lib/utils";
+import { Job } from "@prisma/client";
+import { useEffect, useState } from "react";
 
-const JobsPage = async () => {
-  const allJobs = await prisma.job.findMany({
-    where: {
-      state: "ACTIVE",
-    },
-  });
+const JobsPage = () => {
+    const [jobs, setJobs] = useState<Job[]>([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+      const fetchJobs = async () =>{
+        setLoading(true);
+        const response = await getJobs({});
+        if(response.status === "success"){
+          setJobs(response.data as Job[]);
+        }
+        setLoading(false);
+      };
+      fetchJobs();
+    },[])
 
   return (
     <div className="w-full">
-      {allJobs &&
-        allJobs.map((job) => {
+      {jobs &&
+        jobs.map((job) => {
           return <JobDisplay key={job.id} job={job} />;
         })}
-      {allJobs.length === 0 ? (
+      {jobs.length === 0 ? (
         <h3 className="text-2xl font-semibold text-gray-800">No Jobs Found!</h3>
       ) : null}
     </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,9 @@ import { ChangeEvent, useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Job } from "@prisma/client";
+import { Select, SelectContent, SelectItem, SelectTrigger } from "./ui/select";
+import { SelectValue } from "@radix-ui/react-select";
+import { Slider } from "./ui/slider";
 
 interface SidebarProps {
   setJobs: (jobs: Job[]) => void;
@@ -16,68 +19,129 @@ interface Filters {
   currency: string;
   salRange: [number, number];
 }
+const formatSalary = (value: number, currency: string) => {
+  let formattedValue = value / 1000;
+  let unit = "k";
+
+  if (currency === "INR" || currency === "") {
+    return `${formattedValue} ${unit}`;
+  } else if (currency === "USD") {
+    return `$${formattedValue} ${unit}`;
+  }
+  return `${formattedValue} ${unit}`;
+};
 
 const Sidebar = () => {
-
   const [filters, setFilters] = useState<Filters>({
     title: "",
     companyName: "",
     location: "",
     currency: "",
     salRange: [0, 1000000],
-});
+  });
 
   const handleFilterChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFilters({
-        ...filters,
-        [e.target.name]: e.target.value,
+      ...filters,
+      [e.target.name]: e.target.value,
     });
-};
+  };
 
   const handleSliderChange = (value: [number, number]) => {
     setFilters({
-        ...filters,
-        salRange: value,
+      ...filters,
+      salRange: value,
     });
-};
+  };
 
   const fetchJobs = async () => {
     setLoading(true);
     //@ts-ignore
     const response = await getJobs(filters);
     if (response.status === "success") {
-        //@ts-ignore
-        setJobs(response.data);
+      //@ts-ignore
+      setJobs(response.data);
     }
     setLoading(false);
-};
+  };
 
-useEffect(() => {
+  useEffect(() => {
     fetchJobs();
-}, [filters]);
+  }, [filters]);
 
   return (
-    <div className="font-medium text-xs flex border flex-col items-center p-4 ">
-            <span className="pl-4 text-2xl">
-            Filter
+    <div className="w-[250px] font-medium text-xs flex border flex-col items-center mt-4 pb-4">
+      <span className="text-2xl m-4">Filter Job</span>
+      <div className="flex flex-col gap-y-4 w-60">
+        <Input
+          type="text"
+          name="title"
+          placeholder="Job Title"
+          onChange={handleFilterChange}
+          className="border p-2 rounded-md"
+        />
+        <Input
+          type="text"
+          name="companyName"
+          placeholder="Company Name"
+          onChange={handleFilterChange}
+          className="border p-2 rounded-md"
+        />
+
+        <Select
+          onValueChange={(value) => {
+            setFilters({
+              ...filters,
+              currency: value,
+            });
+          }}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Choose currency" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="INR">₹ INR</SelectItem>
+            <SelectItem value="USD">$ USD</SelectItem>
+          </SelectContent>
+        </Select>
+
+        <Select
+          onValueChange={(value) => {
+            setFilters({
+              ...filters,
+              location: value,
+            });
+          }}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Job Location" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="REMOTE">Remote</SelectItem>
+            <SelectItem value="OFFICE">Office</SelectItem>
+            <SelectItem value="HYBRID">Hybrid</SelectItem>
+          </SelectContent>
+        </Select>
+
+        <div className="flex flex-col gap-2">
+          <Slider
+            defaultValue={filters.salRange}
+            max={1000000}
+            step={1000}
+            onValueChange={handleSliderChange}
+            value={filters.salRange}
+          />
+          <div className="flex justify-between">
+            <span>
+              Min: {formatSalary(filters.salRange[0], filters.currency)}
             </span>
-            <div className="gap-2">
-            <Input
-                    type="text"
-                    name="title"
-                    placeholder="Job Title"
-                    onChange={handleFilterChange}
-                    className="border p-2 rounded-md m-4 "
-                />
-                <Input
-                    type="text"
-                    name="companyName"
-                    placeholder="Company Name"
-                    onChange={handleFilterChange}
-                    className="border p-2 rounded-md m-4"
-                />
-            </div>
+            <span>
+              Max: {formatSalary(filters.salRange[1], filters.currency)}
+            </span>
+          </div>
         </div>
+      </div>
+    </div>
   );
 };
 
@@ -85,4 +149,3 @@ export default Sidebar;
 function setLoading(arg0: boolean) {
   throw new Error("Function not implemented.");
 }
-
