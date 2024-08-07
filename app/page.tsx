abda24c63a4d2a91cc56ac8eea0db28fab41949b
Pheewww <umangsingh67899876@gmail.com>
2024-08-07 06:01:15 +0530

    typo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/Footer";
 import HomePageContent from "../components/HomePage";
 import { SiteHeader } from "@/components/SiteHeader";
 
-const HomePage = async () => {
+const HomePage = () => {
   return (
     <div >
       <SiteHeader />
