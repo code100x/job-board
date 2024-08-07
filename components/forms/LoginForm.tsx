e3f5bbe7c4f2ef214f758d145271be3ec17a9cba
pratiiikkk <neexzz05@gmail.com>
2024-08-07 14:18:40 +0530

    [ IMPROVEMENT ] : Login Page UI and Form Validation #48

--- components/forms/LoginForm.tsx
+++ components/forms/LoginForm.tsx
@@ -2,40 +2,45 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useToast } from "../ui/use-toast";
-import { FormEvent, useState } from "react";
+import { useState } from "react";
 import { loginUser } from "@/actions/user";
 import { useRouter } from "next/navigation";
-import { Loader2 } from "lucide-react";
+import { FaCircleNotch, FaEye, FaEyeSlash, FaGoogle } from "react-icons/fa";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { userLoginSchema } from "@/zod/user";
+import { motion } from "framer-motion";
 
 const LoginForm = () => {
   const { toast } = useToast();
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleClick = async (evt: FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
+  const loginForm = useForm<z.infer<typeof userLoginSchema>>({
+    resolver: zodResolver(userLoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = async (values: z.infer<typeof userLoginSchema>) => {
+    const credentials = values;
     setIsLoading(true);
-    if (!email || email === "") {
-      toast({
-        title: "Email cannot be empty!",
-        variant: "destructive",
-      });
-      setIsLoading(false);
-      return;
-    }
-
-    if (!password || password === "") {
-      toast({
-        title: "Email cannot be empty!",
-        variant: "destructive",
-      });
-      setIsLoading(false);
-      return;
-    }
-
-    const credentials = { email, password };
     const response = await loginUser(credentials);
 
     if (response?.status !== "success") {
@@ -53,46 +58,89 @@ const LoginForm = () => {
     });
     router.push("/jobs");
     setIsLoading(false);
+    console.log(values);
   };
 
   return (
-    <div className="h-fit p-4 bg-white flex flex-col items-start gap-8 rounded-md shadow-lg border-t border-gray-200">
-      <h3 className="max-w-64 flex-wrap text-lg text-gray-700 tracking-tight">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.4 }}
+      className="h-fit w-[500px] p-6 bg-white flex flex-col items-start gap-5 rounded-md shadow-lg border-t border-gray-200"
+    >
+      <h3 className="md:text-lg max-w-80 flex-wrap text-sm text-gray-700 tracking-tight ">
         Login with your 100xdevs to continue.
       </h3>
 
-      <form
-        className="h-full flex flex-col items-center gap-3"
-        onSubmit={(evt) => handleClick(evt)}
-      >
-        <Input
-          value={email}
-          onChange={(evt) => setEmail(evt.target.value)}
-          placeholder="Email"
-          className="w-64 border-gray-400 outline-none text-gray-800"
-        />
+      <div className="w-full max-w-md flex gap-3 flex-col">
+        <Form {...loginForm}>
+          <form onSubmit={loginForm.handleSubmit(handleSubmit)}>
+            <FormField
+              control={loginForm.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="my-4">
+                  <FormLabel className="text-black">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      className=" focus:ring-2 focus:ring-gray-500"
+                      placeholder="example@email.com"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={loginForm.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="my-4">
+                  <FormLabel className="text-black">Password</FormLabel>
+                  <FormControl className="relative ">
+                    <div>
+                      <Input
+                        className="focus:ring-2 focus:ring-gray-500"
+                        type={showPassword ? "password" : "text"}
+                        placeholder="******"
+                        {...field}
+                      />
+                      <div
+                        className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+                        onClick={togglePasswordVisibility}
+                      >
+                        {showPassword ? <FaEye /> : <FaEyeSlash />}
+                      </div>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        <Input
-          type="password"
-          value={password}
-          onChange={(evt) => setPassword(evt.target.value)}
-          placeholder="Password"
-          className="w-64 border-gray-400 outline-none text-gray-800"
-        />
+            <Button type="submit" className="w-full">
+              {isLoading ? (
+                <>
+                  <FaCircleNotch className="animate-spin" />
+                  <span className="ml-3">submit</span>
+                </>
+              ) : (
+                <span>submit</span>
+              )}
+            </Button>
+          </form>
+        </Form>
 
-        <Button
-          disabled={isLoading}
-          type="submit"
-          className="w-full flex justify-center items-center rounded-lg"
-        >
-          {isLoading ? (
-            <Loader2 size={20} className="animate-spin " />
-          ) : (
-            "Sign In"
-          )}
-        </Button>
-      </form>
-    </div>
+        <div className=" w-full">
+          <hr className="mb-3 border-gray border" />
+          <Button variant="outline" className="w-full">
+            <FaGoogle className="mr-2" />
+            Login with Google
+          </Button>
+        </div>
+      </div>
+    </motion.div>
   );
 };
 
--- zod/user.ts
+++ zod/user.ts
@@ -1,8 +1,22 @@
 import { z } from "zod";
 
 export const userLoginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(2),
+  email: z.string().email({
+    message: "Invalid email address",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters long",
+  }),
+  
 });
+
+// export const userRegisterSchema = z.object({
+//   name: z.string().min(2, {
+//     message: "Name must be at least 2 characters long",
+//   }),
+//   email: z.string().email("Invalid email address"),
+//   password: z.string().min(6, "Password must be at least 6 characters long"),
+  
+// });
 
 export type LoginUser = z.infer<typeof userLoginSchema>;
