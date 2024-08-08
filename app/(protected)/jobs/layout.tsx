48de1fbf35147a1399fd260f41e9896af4c71595
Hrithik <hrithikroy1854@gmail.com>
2024-08-08 15:45:16 +0530

    fixed padding issue in mobile-nav

--- app/(protected)/jobs/layout.tsx
+++ app/(protected)/jobs/layout.tsx
@@ -7,7 +7,7 @@ type MainLayoutProps = {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <div className="w-full min-h-screen flex flex-col gap-4 items-center pt-4 pb-20">
+    <div className="w-full min-h-screen flex flex-col gap-4 items-center p-4 pb-20">
       <SiteHeader />
       {children}
     </div>
