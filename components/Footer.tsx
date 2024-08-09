4fe22f8c6b93efa154af7aec8da671123f72411a
karan <karanmali122001@gmail.com>
2024-08-09 20:53:40 +0530

    footer links fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,38 @@
 import { Copyright, Github, GlobeIcon, Linkedin, Twitter } from "lucide-react";
-
+import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="absolute bottom-0 w-full h-20 flex justify-between items-center bg-background p-4 lg:px-14 border border-t-secondary">
       <div className="flex justify-center items-center gap-5 font-normal text-slate-600 :">
-        <Linkedin className="hover:text-gray-800 transition-colors cursor-pointer" />
-        <Twitter className="hover:text-gray-800 transition-colors cursor-pointer" />
-        <Github className="hover:text-gray-800 transition-colors cursor-pointer" />
-        <GlobeIcon className="hover:text-gray-800 transition-colors cursor-pointer" />
+        <Link
+          href="https://in.linkedin.com/company/100xdevs"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <Linkedin className="hover:text-gray-800 transition-colors cursor-pointer" />
+        </Link>
+        <Link
+          href="https://x.com/TechAlerts101"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <Twitter className="hover:text-gray-800 transition-colors cursor-pointer" />
+        </Link>
+        <Link
+          href="https://github.com/code100x/job-board"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          {" "}
+          <Github className="hover:text-gray-800 transition-colors cursor-pointer" />
+        </Link>
+        <Link
+          href="https://100xdevs.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <GlobeIcon className="hover:text-gray-800 transition-colors cursor-pointer" />{" "}
+        </Link>
       </div>
       <span className="flex text-xs font-normal text-slate-600 items-center gap-1">
         <Copyright size={14} />
