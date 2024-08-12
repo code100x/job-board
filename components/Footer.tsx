f2bde1c43429bb819dede8e4d728cd6e29e06b84
Ramnaresh <ramnareshkushwah823@gmail.com>
2024-08-12 19:32:32 +0530

    added github and other social links in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,23 @@ const Footer = () => {
   return (
     <footer className="absolute bottom-0 w-full h-20 flex justify-between items-center bg-background p-4 lg:px-14 border border-t-secondary">
       <div className="flex justify-center items-center gap-5 font-normal text-slate-600 :">
-        <Linkedin className="hover:text-gray-800 transition-colors cursor-pointer" />
-        <Twitter className="hover:text-gray-800 transition-colors cursor-pointer" />
-        <Github className="hover:text-gray-800 transition-colors cursor-pointer" />
-        <GlobeIcon className="hover:text-gray-800 transition-colors cursor-pointer" />
+        <a href="" target="_blank" rel="noopener noreferrer" title="LinkedIn">
+          <Linkedin className="hover:text-gray-800 transition-colors cursor-pointer" />
+        </a>
+        <a href="" target="_blank" rel="noopener noreferrer" title="Twitter">
+          <Twitter className="hover:text-gray-800 transition-colors cursor-pointer" />
+        </a>
+        <a
+          href="https://github.com/Harit007x/job-board"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Github"
+        >
+          <Github className="hover:text-gray-800 transition-colors cursor-pointer" />
+        </a>
+        <a href="" target="_blank" rel="noopener noreferrer" title="Website">
+          <GlobeIcon className="hover:text-gray-800 transition-colors cursor-pointer" />
+        </a>
       </div>
       <span className="flex text-xs font-normal text-slate-600 items-center gap-1">
         <Copyright size={14} />
