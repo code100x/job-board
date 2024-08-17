0e0adbdf7d1bf59e060fe009cd98af2c84c98f37
Sam-Frost <sambrofrost@gmail.com>
2024-08-17 23:53:26 +0530

    Contrast enhanced for light mode

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -7,7 +7,7 @@ const Loader = ({ size = 8 }: LoaderParams) => {
     <>
       <svg
         aria-hidden="true"
-        className={`w-${size} h-${size} text-gray-200 animate-spin dark:text-gray-600 fill-white`}
+        className={`w-${size} h-${size} text-gray-300 animate-spin dark:text-gray-600 fill-white`}
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
