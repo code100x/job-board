e92c2c054b88eebf1ca9629d318fd1a24103668e
Ankit7rma <ankitsharma290101@gmail.com>
2024-08-23 20:31:28 +0530

    fix(toast): toast-timing-fix

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -5,7 +5,7 @@ import * as React from 'react';
 import type { ToastActionElement, ToastProps } from '@/components/ui/toast';
 
 const TOAST_LIMIT = 1;
-const TOAST_REMOVE_DELAY = 1000000;
+const TOAST_REMOVE_DELAY = 3000;
 
 type ToasterToast = ToastProps & {
   id: string;
