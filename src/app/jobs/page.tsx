92dae40b1d65205544ead5a378cd3758c615deca
Prateek Kumar <prateek.kumar.original@gmail.com>
2024-08-25 17:52:59 +0530

    Fix Pagination error

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -2,14 +2,20 @@ import AllJobs from '@/components/all-jobs';
 import Loader from '@/components/loader';
 import JobFilters from '@/layouts/job-filters';
 import JobsHeader from '@/layouts/jobs-header';
-import { JobQuerySchemaType } from '@/lib/validators/jobs.validator';
+import {
+  JobQuerySchema,
+  JobQuerySchemaType,
+} from '@/lib/validators/jobs.validator';
 import { Suspense } from 'react';
+
 const page = async ({ searchParams }: { searchParams: JobQuerySchemaType }) => {
+  const validatedSearchParams = JobQuerySchema.parse(searchParams);
+
   return (
     <div className="container flex gap-5 pt-5">
-      <JobFilters searchParams={searchParams} />
+      <JobFilters searchParams={validatedSearchParams} />
       <div className="grow">
-        <JobsHeader searchParams={searchParams} />
+        <JobsHeader searchParams={validatedSearchParams} />
         <Suspense
           fallback={
             <div className="flex justify-center items-center h-full gap-5 ">
@@ -17,7 +23,7 @@ const page = async ({ searchParams }: { searchParams: JobQuerySchemaType }) => {
             </div>
           }
         >
-          <AllJobs searchParams={searchParams} />
+          <AllJobs searchParams={validatedSearchParams} />
         </Suspense>
       </div>
     </div>
