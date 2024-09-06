5ff0e16c9369f6f2cac60be6e23fc43cd609a03e
Bhupesh <Bhupesh5676l@gmail.com>
2024-09-06 20:53:39 +0530

    Improved the UI

--- src/components/job.tsx
+++ src/components/job.tsx
@@ -5,12 +5,12 @@ import { Button } from './ui/button';
 
 export const Job = ({ job }: { job: JobType }) => {
   return (
-    <section className="p-5 m-5 grid gap-5 border border-white-200 border-[1px] rounded-3xl min-h-[55vh] min-w-[25vw] hover:shadow-[0_2px_15px_7px_rgba(0,119,255,0.5)] transition-shadow duration-300 shadow-lg w-full max-w-md mx-auto">
+    <section className="p-5 m-5 grid gap-5 border border-white-200 border-[1px] rounded-3xl min-h-[55vh] min-w-[25vw] hover:shadow-[0_2px_15px_7px_rgba(0,119,255,0.5)] transition-shadow duration-300 shadow-lg w-full max-w-md mx-auto bg-[#171717]">
       <div className="grid gap-2">
-        <p className="font-bold text-3xl text-center">
+        <p className="font-bold text-3xl text-center content-center">
           {job?.title || 'Unknown Job Title'}
         </p>
-        <p className="text-xl text-primary/80 text-center">
+        <p className="text-xl text-primary/80 text-center content-center">
           {job?.companyName || 'Unknown Company'}
         </p>
         <div className="flex gap-2 justify-center text-center items-center">
