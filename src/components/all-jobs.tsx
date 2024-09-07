9b54fd03ea42fd50c57017b269fe0a58fee6bf1d
Arafat <arafatshaikh823@gmail.com>
2024-09-07 22:21:22 +0530

    Fix scrolling issue by adding scrollbar that affects job listings only

--- src/components/all-jobs.tsx
+++ src/components/all-jobs.tsx
@@ -11,6 +11,7 @@ import Icon from './ui/icon';
 import { formatSalary } from '@/lib/utils';
 import Link from 'next/link';
 import APP_PATHS from '@/config/path.config';
+
 type PaginatorProps = {
   searchParams: JobQuerySchemaType;
 };
@@ -18,7 +19,7 @@ type PaginatorProps = {
 const AllJobs = async ({ searchParams }: PaginatorProps) => {
   const jobs = await getAllJobs(searchParams);
   if (!jobs.status) {
-    return <div>Error {jobs.message}</div>;
+    return <div>Error {jobs.message} hello</div>;
   }
   const totalPages =
     Math.ceil((jobs.additional?.totalJobs || 0) / JOBS_PER_PAGE) ||
@@ -26,38 +27,41 @@ const AllJobs = async ({ searchParams }: PaginatorProps) => {
   const currentPage = parseInt(searchParams.page?.toString()) || DEFAULT_PAGE;
   return (
     <div className="bg-background py-4 grid gap-3 w-full">
-      {jobs.additional?.jobs.map((job) => {
-        return (
-          <Link key={job.id} href={`/jobs/${job.id}`}>
-            <div
-              className="w-[94%] mx-auto flex flex-col items-start gap-4 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent"
-              key={job.id}
-            >
-              <div className="flex w-full flex-col gap-2">
-                <p className="font-semibold">{job.title}</p>
-                <p className="text-xs font-medium">{job.companyName}</p>
+      <div className=" overflow-y-auto max-h-[100vh] overflow-x-hidden scrollbar-hide">
+        {jobs.additional?.jobs.map((job) => {
+          return (
+            <Link key={job.id} href={`/jobs/${job.id}`}>
+              <div
+                className="w-[94%] mx-auto flex flex-col items-start gap-4 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent"
+                key={job.id}
+              >
+                <div className="flex w-full flex-col gap-2">
+                  <p className="font-semibold">{job.title}</p>
+                  <p className="text-xs font-medium">{job.companyName}</p>
+                </div>
+                <div className="flex gap-2 text-xs text-muted-foreground">
+                  <span className="flex items-center gap-0.5">
+                    <Icon icon="location" size={12} />
+                    {job.location}{' '}
+                    <span className="capitalize">({job.workMode})</span>
+                  </span>
+                  <span className="flex items-center gap-0.5">
+                    {job.minSalary && <Icon icon="currency" size={12} />}
+                    {job.minSalary && job.maxSalary
+                      ? `${formatSalary(job.minSalary)}-${formatSalary(job.maxSalary)}`
+                      : 'Not disclosed'}
+                  </span>
+                </div>
+                <p className="flex gap-0.5 items-center text-muted-foreground text-xs">
+                  <Icon icon="description" size={12} />
+                  <span>{job.description}</span>
+                </p>
               </div>
-              <div className="flex gap-2 text-xs text-muted-foreground">
-                <span className="flex items-center gap-0.5">
-                  <Icon icon="location" size={12} />
-                  {job.location}{' '}
-                  <span className="capitalize">({job.workMode})</span>
-                </span>
-                <span className="flex items-center gap-0.5">
-                  {job.minSalary && <Icon icon="currency" size={12} />}
-                  {job.minSalary && job.maxSalary
-                    ? `${formatSalary(job.minSalary)}-${formatSalary(job.maxSalary)}`
-                    : 'Not disclosed'}
-                </span>
-              </div>
-              <p className="flex gap-0.5 items-center text-muted-foreground text-xs">
-                <Icon icon="description" size={12} />
-                <span>{job.description}</span>
-              </p>
-            </div>
-          </Link>
-        );
-      })}
+            </Link>
+          );
+        })}
+      </div>
+
       <Pagination>
         <PaginationContent>
           {totalPages ? (
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,7 +102,7 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar-hide')],
 } satisfies Config;
 
 export default config;
