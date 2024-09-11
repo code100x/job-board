13719cef62f96846ef9390ab967493f3b0a522b1
praneash2 <praneash2@gmail.com>
2024-09-11 19:59:16 +0530

    Improvement: UI improvement of /jobs page

--- src/components/job.tsx
+++ src/components/job.tsx
@@ -1,28 +1,35 @@
+"use client"
 import { JobType } from '@/types/jobs.types';
 import Icon from './ui/icon';
 import { formatSalary } from '@/lib/utils';
 import { Button } from './ui/button';
-
+import {  formatDistanceToNow } from 'date-fns';
 export const Job = ({ job }: { job: JobType }) => {
+  
+ 
+  
   return (
-    <section className="grid gap-5 border p-3 rounded-lg">
-      <div className="grid gap-2">
-        <p className="font-bold text-2xl ">{job.title}</p>
+    <section className="flex flex-col items-center gap-5 border p-3 rounded-lg">
+      <div className="flex gap-2 flex-col items-center">
         <p className="text-xl text-primary/80">{job.companyName}</p>
-        <div className="flex gap-2">
-          <span className="flex items-center gap-0.5">
+        <p className="font-bold text-2xl ">{job.title}</p>
+        <div className="flex flex-col items-center gap-2 mt-3">
+          <span className="flex gap-0.5 items-center">
             <Icon icon="location" size={12} />
             {job.workMode}
           </span>
-          <span className="flex items-center gap-0.5">
+          <div className="flex items-center gap-0.5">
             {job.minSalary && <Icon icon="currency" size={12} />}
             {job.minSalary && job.maxSalary
               ? `${formatSalary(job.minSalary)}-${formatSalary(job.maxSalary)}`
               : 'Not disclosed'}
-          </span>
+          </div>
+          <p className="job-posted-time">
+          {formatDistanceToNow(job.postedAt, { addSuffix: true })}
+          </p>
         </div>
       </div>
-      <Button className="justify-self-start">Apply</Button>
+      <Button className="justify-self-start rounded-lg">Apply</Button>
       <p>{job.description}</p>
     </section>
   );
