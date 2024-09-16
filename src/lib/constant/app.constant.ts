8f382d61a2c6111b8806701b5455b304615ed1e6
Farhan <144435177+toufiqfarhan0@users.noreply.github.com>
2024-09-17 00:51:52 +0530

    added a post job button on the landing page (#329)
    * added a post job button on the landing page
    
    * renamed to create job
    
    ---------
    
    Co-authored-by: Vineet Agarwal <91052168+VineeTagarwaL-code@users.noreply.github.com>

--- src/lib/constant/app.constant.ts
+++ src/lib/constant/app.constant.ts
@@ -14,7 +14,8 @@ export const navbar = [
   { id: 3, label: 'Internship', path: '/' },
   { id: 4, label: 'Testimonials', path: '/' },
   { id: 5, label: 'FAQs', path: '/' },
-  { id: 6, label: 'Update Details', path: '/editDetails' },
+  { id: 6, label: 'Post a Job', path: APP_PATHS.POST_JOB },
+
 ];
 
 export const socials: {
