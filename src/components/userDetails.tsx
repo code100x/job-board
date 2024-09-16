85bf46309460e76cc9ae7200d3600ea5fbe93191
rishipatel9 <rishipatel0826@gmail.com>
2024-09-16 13:39:09 +0530

    Updated Ui

--- src/components/userDetails.tsx
+++ src/components/userDetails.tsx
@@ -4,50 +4,32 @@ import { Button } from './ui/button';
 
 const UserDetails = () => {
   return (
-    <div className="h-[100vh] flex mt-14 items-center md:items-start flex-col md:flex-row  md:justify-center ">
-      <div className="max-w-[300px] w-[350px] flex flex-col gap-4 items-center p-4">
-        <div>
-          <Button className="w-[200px] m-auto">Applied to</Button>
-        </div>
-        <div>
-          <Button className="w-[200px] m-auto">Update Details</Button>
-        </div>
-      </div>
-      <div>
-        <div className="flex flex-col max-w-[600px] gap-3 mx-8">
-          <div className="flex">
-            <div>
-              {' '}
-              <Input value={''} placeholder="First name" />
-            </div>
-            <div>
-              {' '}
-              <Input value={''} placeholder="Last name" />
-            </div>
+    <main className="flex justify-center items-center h-screen ">
+      <section className=" py-12 md:py-24 lg:py-32    ">
+        <div className="container px-4 md:px-6 bg-transparent backdrop-blur-md text-white border-[#27272B] border   ">
+          <div className="text-2xl font-bold tracking-tighter sm:text-4xl md:text-4xl text-white m-4 text-center ">
+            Update Details
           </div>
-          <div>
-            <Input placeholder="Github" />
+          <div className="flex sm:w-full flex gap-2 ">
+            <Input placeholder="First Name"></Input>
+            <Input placeholder="Last Name"></Input>
           </div>
-          <div>
-            <Input placeholder="LinkedIn" />
+          <div className="flex sm:w-full flex flex-col gap-2 mt-2">
+            <Input placeholder="Github"></Input>
+            <Input placeholder="LinkedIn"></Input>
+            <Input placeholder="Twitter"></Input>
+            <Input placeholder="LPA"></Input>
           </div>
-          <div>
-            <Input placeholder="Twitter" />
-          </div>
-          <div>
-            <Button value={'Upload Resume'}>Upload Resume </Button>
-          </div>
-          <div className="flex justify-between">
-            <div>
-              <Input placeholder="LPA" className="max-w-[135px]" />
-            </div>
-            <div>
-              <Button>Submit</Button>
-            </div>
+
+          <div className="flex mt-2 sm:flex  flex flex-col gap-2 mt-2 ">
+            <Button className="w-full sm:w-auto  animate-fade-in-up bg-white text-black hover:bg-gray-300">Upload Resume</Button>
+            <Button className="w-full sm:w-auto  animate-fade-in-up bg-white text-black hover:bg-gray-300">Applied To</Button>
+            <Button className="w-full sm:w-auto animate-fade-in-up bg-white text-black hover:bg-gray-300">Submit</Button>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </main>
+
   );
 };
 
