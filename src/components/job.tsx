a0d1fbafe570cfa307366f69889bb11808dbbdf1
Satyam <raisatyam121@gmail.com>
2024-09-20 21:17:09 +0530

    feat: make links clickable in job description (#373)
    Co-authored-by: Vineet Agarwal <91052168+VineeTagarwaL-code@users.noreply.github.com>

--- src/components/job.tsx
+++ src/components/job.tsx
@@ -2,7 +2,11 @@ import { JobType } from '@/types/jobs.types';
 import Icon from './ui/icon';
 import { formatSalary } from '@/lib/utils';
 import { Button } from './ui/button';
-
+import Linkify from 'linkify-react';
+const options = {
+  defaultProtocol: 'https',
+  target: '_blank',
+};
 export const Job = ({ job }: { job: JobType }) => {
   return (
     <section className="grid gap-5 border p-3 rounded-lg">
@@ -23,7 +27,9 @@ export const Job = ({ job }: { job: JobType }) => {
         </div>
       </div>
       <Button className="justify-self-start">Apply</Button>
-      <p>{job.description}</p>
+      <Linkify options={options}>
+        <p>{job.description}</p>
+      </Linkify>
     </section>
   );
 };
