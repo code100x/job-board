01b27aa728491c62d2fd36c5215aa7dd03b368bc
nolostra <saiesh1400@gmail.com>
2024-09-20 16:21:13 +0530

    fix: working changes

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ import createJiti from "jiti";
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
 // Import env here to validate during build. Using jiti we can import .ts files :)
-jiti("./src/env");
+jiti("./src/env/client");
+jiti("./src/env/server");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
