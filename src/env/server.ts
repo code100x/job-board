2d52ab922e45d611748a22550d636f717fdb7d96
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-09-29 01:54:54 +0530

    chore: removed t3-env checker temp

--- src/env/server.ts
+++ src/env/server.ts
@@ -1,22 +1,22 @@
-// server-env.ts
-import { createEnv } from '@t3-oss/env-nextjs';
-import { z } from 'zod';
+// // server-env.ts
+// import { createEnv } from '@t3-oss/env-nextjs';
+// import { z } from 'zod';
 
-export const serverEnv = createEnv({
-  server: {
-    DATABASE_URL: z.string().url(),
-    NEXTAUTH_SECRET: z.string().min(1),
-    NEXTAUTH_URL: z.string().url(),
-    CDN_API_KEY: z.string().min(1),
-    CDN_BASE_UPLOAD_URL: z.string().url(),
-    CDN_BASE_ACCESS_URL: z.string().url(),
-  },
-  runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    CDN_API_KEY: process.env.CDN_API_KEY,
-    CDN_BASE_UPLOAD_URL: process.env.CDN_BASE_UPLOAD_URL,
-    CDN_BASE_ACCESS_URL: process.env.CDN_BASE_ACCESS_URL,
-  },
-});
+// export const serverEnv = createEnv({
+//   server: {
+//     DATABASE_URL: z.string().url(),
+//     NEXTAUTH_SECRET: z.string().min(1),
+//     NEXTAUTH_URL: z.string().url(),
+//     CDN_API_KEY: z.string().min(1),
+//     CDN_BASE_UPLOAD_URL: z.string().url(),
+//     CDN_BASE_ACCESS_URL: z.string().url(),
+//   },
+//   runtimeEnv: {
+//     DATABASE_URL: process.env.DATABASE_URL,
+//     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+//     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+//     CDN_API_KEY: process.env.CDN_API_KEY,
+//     CDN_BASE_UPLOAD_URL: process.env.CDN_BASE_UPLOAD_URL,
+//     CDN_BASE_ACCESS_URL: process.env.CDN_BASE_ACCESS_URL,
+//   },
+// });
