b68e70076fa1710347697a98cb48965db378bd0e
Dev <dev2005suthar@gmail.com>
2024-10-05 00:40:42 +0530

    made isVerfied field optional jobType

--- src/types/jobs.types.ts
+++ src/types/jobs.types.ts
@@ -16,7 +16,7 @@ export type JobType = {
   description: string | null;
   companyName: string;
   postedAt: Date;
-  isVerifiedJob: Boolean;
+  isVerifiedJob?: Boolean;
 };
 export type getAllJobsAdditonalType = {
   jobs: JobType[];
