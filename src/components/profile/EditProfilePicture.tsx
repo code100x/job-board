6f754c30e588f38dc7c271b27c6e2cc7a54c9ffa
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-05 14:53:00 +0530

    Refactor profile picture upload functionality

--- src/components/profile/EditProfilePicture.tsx
+++ src/components/profile/EditProfilePicture.tsx
@@ -1,54 +1,68 @@
 'use client';
-import APP_PATHS from '@/config/path.config';
+
+import React, { useEffect, useTransition } from 'react';
+
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import React, { useEffect } from 'react';
+import { useToast } from '../ui/use-toast';
+
+import { uploadFileAction } from '@/actions/upload-to-cdn';
+import { updateAvatar } from '@/actions/user.profile.actions';
+
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
+
 import { PencilIcon, Trash } from 'lucide-react';
-import { uploadFileAction } from '@/actions/upload-to-cdn';
-import { updateAvatar } from '@/actions/user.profile.actions';
-import { useToast } from '../ui/use-toast';
+import APP_PATHS from '@/config/path.config';
+
 type Props = {
   imageUrl?: string;
   name: string;
 };
+
 export const EditProfilePicture = ({ imageUrl, name }: Props) => {
   const router = useRouter();
   const session = useSession();
   const { toast } = useToast();
+  const [file, setFile] = React.useState<File | null>(null);
+
+  const [isPending, startTransition] = useTransition();
 
   const uploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (!file) return;
-    const formData = new FormData();
-    formData.append('file', file);
-    const response = await uploadFileAction(formData);
-    await updateAvatar(session.data?.user.email || '', response?.url || '');
-    if (response.error) {
-      toast({
-        title: response.error,
-        variant: 'destructive',
+    startTransition(() => {
+      const file = e.target.files?.[0];
+      if (!file) return;
+      setFile(file);
+
+      const formData = new FormData();
+
+      formData.append('file', file);
+
+      uploadFileAction(formData).then((response) => {
+        if (response.error) {
+          toast({
+            title: response.error,
+            variant: 'destructive',
+          });
+        } else {
+          updateAvatar(
+            session.data?.user.email as string,
+            response?.url as string
+          ).then((res) => {
+            res.error
+              ? toast({
+                  title: res.error as string,
+                  variant: 'destructive',
+                })
+              : toast({
+                  title: res.success,
+                  variant: 'success',
+                });
+          });
+        }
       });
-    } else {
-      const res = await updateAvatar(
-        session.data?.user.email || '',
-        response?.url || ''
-      );
-      await session.update({ ...session, user: { image: response.url } });
-      if (res.error) {
-        toast({
-          title: res.error as string,
-          variant: 'destructive',
-        });
-      } else {
-        toast({
-          title: res.success,
-          variant: 'success',
-        });
-      }
-    }
+    });
   };
 
   useEffect(() => {
@@ -60,7 +74,10 @@ export const EditProfilePicture = ({ imageUrl, name }: Props) => {
     <div className="flex flex-col items-center justify-center gap-4 my-4">
       <div className="flex justify-center items-center">
         <Avatar className="cursor-pointer w-64 h-64">
-          <AvatarImage className=" object-cover" src={imageUrl || ''} />
+          <AvatarImage
+            className=" object-cover"
+            src={imageUrl || (file ? URL.createObjectURL(file) : undefined)}
+          />
           <AvatarFallback className="text-8xl font-bold">{name}</AvatarFallback>
         </Avatar>
       </div>
@@ -70,8 +87,14 @@ export const EditProfilePicture = ({ imageUrl, name }: Props) => {
           className="flex items-center justify-center gap-3 text-xs bg-slate-950 text-white dark:bg-white dark:text-black rounded-full px-6 cursor-pointer"
           htmlFor="pfp"
         >
-          <PencilIcon className="w-4 h-4" />
-          Change
+          {isPending ? (
+            'Uploading...'
+          ) : (
+            <>
+              <PencilIcon className="w-4 h-4" />
+              Change
+            </>
+          )}
         </Label>
         <input
           type="file"
@@ -81,9 +104,11 @@ export const EditProfilePicture = ({ imageUrl, name }: Props) => {
           accept="image/jpeg, image/jpg, image/png"
           max={1}
           onChange={uploadImage}
+          disabled={isPending}
         />
         <Button
           variant={'ghost'}
+          disabled={isPending}
           className="flex items-center justify-center gap-3 text-xs text-red-400 bg-none border-none bg-transparent hover:bg-transparent"
         >
           <Trash className="w-4 h-4" />
