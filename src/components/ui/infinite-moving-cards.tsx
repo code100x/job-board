0a5127887110c94ba9e7bc9e04764dfcf07dd8c7
Dhirajsingh212 <dhirajsingh0229@gmail.com>
2024-10-07 23:25:44 +0530

    updates ui

--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -26,6 +26,14 @@ export const InfiniteMovingCards = ({
   const [start, setStart] = useState(false);
   function addAnimation() {
     if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
       getDirection();
       getSpeed();
       setStart(true);
