a052fcdeb64e54c2c43e93d530d6e78c678c43d9
Paribesh Nepal <nepalparibesh01@gmail.com>
2024-10-10 12:39:49 +0530

    feat:Added actions for the update and delete

--- src/actions/company.actions.ts
+++ src/actions/company.actions.ts
@@ -3,7 +3,12 @@ import prisma from '@/config/prisma.config';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/authOptions';
 import { ErrorHandler } from '@/lib/error';
-import { CompanySchema } from '@/lib/validators/companies.validator';
+import {
+  CompanySchema,
+  CompanyUpdateSchema,
+  CompanyUpdateSchemaType,
+} from '@/lib/validators/companies.validator';
+import { CompanySchemaType } from '@/types/company.types';
 
 export const getAllCompanies = async () => {
   const auth = await getServerSession(authOptions);
@@ -29,7 +34,7 @@ export const getAllCompanies = async () => {
   };
 };
 
-export const createCompany = async (data: any) => {
+export const createCompany = async (data: CompanySchemaType) => {
   const auth = await getServerSession(authOptions);
   if (!auth || !auth?.user?.id)
     throw new ErrorHandler('Not Authorized', 'UNAUTHORIZED');
@@ -52,5 +57,69 @@ export const createCompany = async (data: any) => {
     data: {
       company,
     },
+  };
+};
+
+export const updateCompany = async (
+  id: string,
+  data: CompanyUpdateSchemaType
+) => {
+  const auth = await getServerSession(authOptions);
+  if (!auth || !auth?.user?.id) {
+    throw new ErrorHandler('Not Authorized', 'UNAUTHORIZED');
+  }
+
+  // Validate the incoming data against the schema
+  const result = CompanyUpdateSchema.parse(data);
+  const { name, email, website, bio, logo } = result;
+
+  const company = await prisma.company.update({
+    where: {
+      id: id,
+      userId: auth.user.id,
+    },
+    data: {
+      name,
+      email,
+      website,
+      bio,
+      logo,
+    },
+  });
+
+  return {
+    msg: 'Company has been successfully updated.',
+    status: 200,
+    data: {
+      company,
+    },
+  };
+};
+
+export const deleteCompany = async (id: string) => {
+  const auth = await getServerSession(authOptions);
+  if (!auth || !auth?.user?.id) {
+    throw new ErrorHandler('Not Authorized', 'UNAUTHORIZED');
+  }
+
+  const company = await prisma.company.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!company || company.userId !== auth.user.id) {
+    throw new ErrorHandler('Company not found or not authorized', 'NOT_FOUND');
+  }
+
+  await prisma.company.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  return {
+    msg: 'Company has been successfully deleted.',
+    status: 200,
   };
 };
--- src/lib/validators/companies.validator.ts
+++ src/lib/validators/companies.validator.ts
@@ -12,5 +12,19 @@ export const CompanyByIdSchema = z.object({
   id: z.string().min(1, 'Job id is required'),
 });
 
+export const CompanyUpdateSchema = z.object({
+  name: z.string().min(1, 'Name is required').optional(),
+  email: z
+    .string()
+    .email('Email is invalid')
+    .min(1, 'Email is required')
+    .optional(),
+  website: z.string().optional(),
+  bio: z.string().min(1, 'Description is required').optional(),
+  logo: z.string().url().optional(),
+});
+
+// Types
 export type CompanySchemaType = z.infer<typeof CompanySchema>;
+export type CompanyUpdateSchemaType = z.infer<typeof CompanyUpdateSchema>;
 export type CompanyByIdSchemaType = z.infer<typeof CompanyByIdSchema>;
