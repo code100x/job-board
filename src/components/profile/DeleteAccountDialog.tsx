2bc45654a513cc5cabba47d726f3bca9519d9807
kashyap1ankit <kashyap25ankit@gmail.com>
2024-10-18 01:24:06 +0530

    added the checks before deleting account

--- src/components/profile/DeleteAccountDialog.tsx
+++ src/components/profile/DeleteAccountDialog.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import { useTransition } from 'react';
+import { ClipboardEvent, useEffect, useState, useTransition } from 'react';
 
 import { signOut, useSession } from 'next-auth/react';
 import { useToast } from '../ui/use-toast';
 
 import { Button } from '../ui/button';
-import Loader from '../loader';
 
 import {
   Dialog,
@@ -15,17 +14,43 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-  DialogClose,
 } from '@/components/ui/dialog';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Randomstring from 'randomstring';
+import {
+  UserProfileDestroySchema,
+  UserProfileDestroyType,
+} from '@/lib/validators/user.profile.validator';
 
 import { deleteUser } from '@/actions/user.profile.actions';
 import { Trash } from 'lucide-react';
+import { FaSpinner } from 'react-icons/fa';
 
 export const DeleteAccountDialog = () => {
   const { toast } = useToast();
   const session = useSession();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserProfileDestroyType>({
+    resolver: zodResolver(UserProfileDestroySchema),
+  });
+
   const [isPending, startTransition] = useTransition();
+  const [modalOpen, setModalOpen] = useState(false);
+  const [randomString, setRandomString] = useState<string>('');
+
+  const [disabled, setDisabled] = useState(true);
+
+  function handleCheck(e: any) {
+    if (e.target.value === randomString) {
+      return setDisabled(false);
+    }
+    setDisabled(true);
+  }
 
   const handleDeleteAccount = async () => {
     startTransition(() => {
@@ -46,10 +71,19 @@ export const DeleteAccountDialog = () => {
         });
     });
   };
+
+  useEffect(() => {
+    setRandomString(Randomstring.generate(8));
+  }, []);
+
   return (
-    <Dialog>
+    <Dialog open={modalOpen}>
       <DialogTrigger asChild>
-        <Button className="w-40 flex gap-2 text-red-500" variant={'ghost'}>
+        <Button
+          className="w-40 flex gap-2 text-red-500"
+          variant={'ghost'}
+          onClick={() => setModalOpen(true)}
+        >
           <Trash size={15} />
           Delete Account
         </Button>
@@ -62,23 +96,43 @@ export const DeleteAccountDialog = () => {
           <DialogDescription>
             This action cannot be undone. This will permanently delete your
             account and all associated data.
+            <form className="mt-6" onSubmit={handleSubmit(handleDeleteAccount)}>
+              <label className="text-gray-200" htmlFor="random">
+                Type{' '}
+                <span className="text-white font-bold">{randomString}</span>
+              </label>
+              <input
+                {...register('random')}
+                id="random"
+                className=" mt-2 p-2 rounded-md text-white p-4 rounded-md  w-full bg-inputBg outline-none"
+                onPaste={(e: ClipboardEvent<HTMLInputElement>) =>
+                  e.preventDefault()
+                }
+                onChange={handleCheck}
+              />
+
+              {errors.random?.message && (
+                <p className="mt-2 text-red-500">{errors.random?.message}</p>
+              )}
+
+              <div className="flex gap-2 items-baseline">
+                <Button
+                  disabled={disabled}
+                  className="mt-4 bg-red-500 hover:bg-red-500 text-white"
+                >
+                  {isPending ? <FaSpinner className="animate-spin" /> : 'Yes'}
+                </Button>
+
+                <div
+                  className="bg-transparent text-white hover:bg-transparent border-2 border-slate-500 py-2 px-4 cursor-pointer rounded-md"
+                  onClick={() => setModalOpen(false)}
+                >
+                  <p>No</p>
+                </div>
+              </div>
+            </form>
           </DialogDescription>
         </DialogHeader>
-        <div className="flex gap-4 justify-end">
-          <Button
-            className="w-36"
-            variant={'destructive'}
-            onClick={handleDeleteAccount}
-            disabled={isPending}
-          >
-            {isPending ? <Loader /> : 'Delete Account'}
-          </Button>
-          <DialogClose asChild>
-            <Button variant={'ghost'} disabled={isPending}>
-              Cancel
-            </Button>
-          </DialogClose>
-        </div>
       </DialogContent>
     </Dialog>
   );
