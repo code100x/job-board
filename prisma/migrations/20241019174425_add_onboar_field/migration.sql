c152c8c435b9621c18a75a519f85ab95a2f2e2ad
Vineet Agarwal <91052168+VineeTagarwaL-code@users.noreply.github.com>
2024-10-19 23:20:01 +0530

    [fix]: fix multistep form (#519)

---
+++ prisma/migrations/20241019174425_add_onboar_field/migration.sql
@@ -0,0 +1,39 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "onBoard" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "resume" TEXT,
+ADD COLUMN     "skills" TEXT[];
+
+-- CreateTable
+CREATE TABLE "Experience" (
+    "id" SERIAL NOT NULL,
+    "companyName" TEXT NOT NULL,
+    "designation" TEXT NOT NULL,
+    "EmploymentType" "EmployementType" NOT NULL,
+    "address" TEXT NOT NULL,
+    "workMode" "WorkMode" NOT NULL,
+    "currentWorkStatus" BOOLEAN NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3),
+    "description" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Experience_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "projectName" TEXT NOT NULL,
+    "projectSummary" TEXT NOT NULL,
+    "projectLiveLink" TEXT,
+    "projectGithub" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Experience" ADD CONSTRAINT "Experience_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
