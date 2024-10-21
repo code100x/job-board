fd309e6f65fe7e4d7cf23c4070eb018367a4f1ad
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-21 14:06:23 +0530

    Refactor profile pages: Add "Add more" functionality

--- src/app/profile/experience/page.tsx
+++ src/app/profile/experience/page.tsx
@@ -1,5 +1,13 @@
 'use client';
 import { UserExperience } from '@/components/profile/UserExperience';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { AddExperience } from '@/components/user-multistep-form/addExperience-form';
 import APP_PATHS from '@/config/path.config';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
@@ -14,8 +22,19 @@ export default function AccountExperiencePage() {
   }, [session.status, router]);
   return (
     <div className="md:container flex flex-col w-full gap-4">
-      <div className="flex justify-between items-center">
+      <div className="flex md:justify-between justify-start items-center gap-5">
         <span>Experience</span>
+        <Dialog>
+          <DialogTrigger>Add more</DialogTrigger>
+          <DialogContent className="max-w-md max-h-[90vh] flex flex-col p-0">
+            <DialogHeader className="p-6 pb-2">
+              <DialogTitle>Add Experience</DialogTitle>
+            </DialogHeader>
+            <div className="flex-grow overflow-y-auto px-6 pb-6">
+              <AddExperience />
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
       <UserExperience />
     </div>
--- src/app/profile/skills/page.tsx
+++ src/app/profile/skills/page.tsx
@@ -1,5 +1,13 @@
 'use client';
 import { UserSkills } from '@/components/profile/UserSkills';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { AddSkills } from '@/components/user-multistep-form/add-skills-form';
 import APP_PATHS from '@/config/path.config';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
@@ -14,8 +22,19 @@ export default function AccountResumePage() {
   }, [session.status, router]);
   return (
     <div className="md:container flex flex-col w-full gap-4">
-      <div className="flex justify-between items-center">
+      <div className="flex md:justify-between justify-start items-center gap-5">
         <span>Skills</span>
+        <Dialog>
+          <DialogTrigger>Add more</DialogTrigger>
+          <DialogContent className="max-w-md max-h-[90vh] flex flex-col p-0">
+            <DialogHeader className="p-6 pb-2">
+              <DialogTitle>Add Skills</DialogTitle>
+            </DialogHeader>
+            <div className="flex-grow overflow-y-auto px-6 pb-6">
+              <AddSkills />
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
       <UserSkills />
     </div>
