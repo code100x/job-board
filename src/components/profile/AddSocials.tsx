b5cbe0a1f3745b80e32079bc75f6d17f3a961dd8
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-22 13:06:07 +0530

    Refactor AddSocials component to include social media links management and validation

--- src/components/profile/AddSocials.tsx
+++ src/components/profile/AddSocials.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useTransition } from 'react';
 
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
@@ -12,11 +12,11 @@ import { Input } from '@/components/ui/input';
 import { Button } from '../ui/button';
 
 import {
-  UserProfileSchema,
-  UserProfileSchemaType,
+  userSocialSchema,
+  userSocialSchemaType,
 } from '@/lib/validators/user.profile.validator';
 
-// import { useToast } from '../ui/use-toast';
+import { useToast } from '../ui/use-toast';
 
 import {
   Form,
@@ -26,78 +26,114 @@ import {
   FormLabel,
   FormMessage,
 } from '../ui/form';
+import {
+  addUserSocials,
+  deleteUserSocials,
+} from '@/actions/user.profile.actions';
 
-// import Loader from '../loader';
+import Loader from '../loader';
+import { Trash } from 'lucide-react';
 type Props = {
-  name: string;
-  email: string;
+  socials?: userSocialSchemaType;
 };
-export const AddSocials = ({}: Props) => {
+export const AddSocials = ({ socials }: Props) => {
   const router = useRouter();
   const session = useSession();
-  // const { toast } = useToast();
+  const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
 
-  // const [isPending, startTransition] = useTransition();
-
-  const form = useForm<UserProfileSchemaType>({
-    resolver: zodResolver(UserProfileSchema),
+  const form = useForm<userSocialSchemaType>({
+    resolver: zodResolver(userSocialSchema),
     defaultValues: {
-      name: '',
-      email: '',
+      platform: socials?.platform || '',
+      link: socials?.link || '',
     },
   });
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    form.setValue(e.target.name as keyof UserProfileSchemaType, e.target.value);
+    form.setValue(e.target.name as keyof userSocialSchemaType, e.target.value);
   };
 
-  // const handleFormSubmit = async (data: UserProfileSchemaType) => {
-  //     try {
-  //         startTransition(() => {
-  //             updateUser(data)
-  //                 .then((res) => {
-  //                     res.error
-  //                         ? toast({
-  //                             title: (res.error as string) || 'something went wrong',
-  //                             variant: 'destructive',
-  //                         })
-  //                         : toast({ title: res.success as string, variant: 'success' });
-  //                 })
-  //                 .then(() => {
-  //                     session.update({ ...session, user: { ...user, ...data } });
-  //                 });
-  //         });
-  //     } catch (error: any) {
-  //         toast({
-  //             title: error?.message || 'Internal server error',
-  //             variant: 'destructive',
-  //         });
-  //     }
-  // };
+  const handleFormSubmit = async (data: userSocialSchemaType) => {
+    try {
+      startTransition(() => {
+        addUserSocials(data).then((res) => {
+          if (res.status) {
+            toast({
+              title: 'Socials added successfully',
+              variant: 'success',
+            });
+            form.reset();
+          } else {
+            toast({
+              title: res.message || 'Internal server error',
+              variant: 'destructive',
+            });
+          }
+        });
+      });
+    } catch (error: any) {
+      toast({
+        title: error?.message || 'Internal server error',
+        variant: 'destructive',
+      });
+    }
+  };
 
+  const delteteSocial = async (id: string) => {
+    try {
+      startTransition(() => {
+        deleteUserSocials({ socialId: id }).then((res) => {
+          if (res.status) {
+            toast({
+              title: 'Socials deleted successfully',
+              variant: 'success',
+            });
+          } else {
+            toast({
+              title: res.message || 'Internal server error',
+              variant: 'destructive',
+            });
+          }
+        });
+      });
+    } catch (error: any) {
+      toast({
+        title: error?.message || 'Internal server error',
+        variant: 'destructive',
+      });
+    }
+  };
   useEffect(() => {
     if (session.status !== 'loading' && session.status === 'unauthenticated')
       router.push(`${APP_PATHS.SIGNIN}?redirectTo=/profile/edit`);
   });
+
   return (
     <Form {...form}>
-      <form className="flex flex-col gap-3 p-4 border rounded-md w-full">
-        <div className="flex justify-between items-center mb-3">
-          <span>Add Socials</span>
-        </div>
-        <div className="flex lg:flex-row md:flex-col w-full gap-3 max-md:items-end lg:items-end max-sm:flex-col">
-          <div className="flex lg:flex-row md:w-full gap-3 w-full max-md:items-end">
-            <div className="md:w-full lg:w-1/3 sm:w-1/2">
+      <form
+        className="flex flex-col gap-3 w-full"
+        onSubmit={form.handleSubmit(handleFormSubmit)}
+      >
+        <div
+          className={`flex ${socials && 'lg:flex-row'} md:flex-col w-full gap-3 max-md:items-end lg:items-end max-sm:flex-col`}
+        >
+          <div
+            className={`flex ${socials && 'lg:flex-row'} md:w-full gap-3 w-full max-md:items-end`}
+          >
+            <div
+              className={`${socials ? 'md:w-full lg:w-1/2 sm:w-1/2' : 'w-1/2'}`}
+            >
               <FormField
                 control={form.control}
-                name="name"
+                name="platform"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Social Platform Name</FormLabel>
                     <FormControl>
                       <Input
                         {...field}
-                        // disabled={isPending}
+                        disabled={isPending}
                         onChange={handleInputChange}
                         className="rounded focus-visible:ring-0 focus:outline-none focus:border-slate-500"
                       />
@@ -107,17 +143,19 @@ export const AddSocials = ({}: Props) => {
                 )}
               />
             </div>
-            <div className="md:w-full w-1/2">
+            <div
+              className={`${socials ? 'md:w-full lg:w-1/2 sm:w-1/2' : 'w-1/2'}`}
+            >
               <FormField
                 control={form.control}
-                name="name"
+                name="link"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Social URL</FormLabel>
                     <FormControl>
                       <Input
                         {...field}
-                        // disabled={isPending}
+                        disabled={isPending}
                         onChange={handleInputChange}
                         className="rounded focus-visible:ring-0 focus:outline-none focus:border-slate-500"
                       />
@@ -128,14 +166,35 @@ export const AddSocials = ({}: Props) => {
               />
             </div>
           </div>
-          <div className="flex justify-end max-sm:w-full">
-            <Button
-              // disabled={isPending}
-              className="bg-slate-950 text-white dark:text-slate-950 dark:bg-white rounded-md py-2 px-4 md:w-56 w-full"
-            >
-              {/* {isPending ? <Loader /> : 'Save'} */}
-              Save
-            </Button>
+          <div className="flex justify-end">
+            {socials ? (
+              <div className="flex gap-3">
+                <Button
+                  disabled={isPending}
+                  className="bg-slate-950 text-white dark:text-slate-950 dark:bg-white rounded-md py-2 px-4"
+                >
+                  {isPending ? <Loader /> : 'Save'}
+                </Button>
+                <Button
+                  variant={'ghost'}
+                  disabled={isPending}
+                  onClick={() =>
+                    socials && socials?.id && delteteSocial(socials.id)
+                  }
+                  className="flex items-center justify-center gap-3 text-xs text-red-400 bg-none border-none bg-transparent hover:bg-transparent"
+                >
+                  <Trash className="w-4 h-4" />
+                  Remove
+                </Button>
+              </div>
+            ) : (
+              <Button
+                disabled={isPending}
+                className={`bg-slate-950 text-white dark:text-slate-950 dark:bg-white rounded-md py-2 px-4 ${!socials && 'md:w-56'} w-full`}
+              >
+                {isPending ? <Loader /> : 'Save'}
+              </Button>
+            )}
           </div>
         </div>
       </form>
